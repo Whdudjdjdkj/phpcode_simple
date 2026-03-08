51e08effb37712dec046b87d0e99dc421fc93288
CAGRI YILDIRIM <yildirimcagrilar@gmail.com>
2026-03-08 18:01:35 +0300

    Update code.php

--- code.php
+++ code.php
@@ -12,4 +12,14 @@
 
  #iki veYa üç rakamlı sayılar
  $desen = '/\d{2,3}/';
+
+ #(serialize) komutu
+ #sonradan geri dönüştürmek için
+ #bir değeri saklar
+ $d = serialize(4);
+
+ #unserialize komutu
+ #bir değeri geri döndürür
+ $dun = unserialize($d);
+ #dosyaya yazma ve dosyadan okuma değeri yeniden kullanma işine yarar.
 ?>
