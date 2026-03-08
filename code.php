4ca22a4bd3c74c42212d30a92a370c8511acaf17
CAGRI YILDIRIM <yildirimcagrilar@gmail.com>
2026-03-08 21:08:45 +0300

    Update code.php

--- code.php
+++ code.php
@@ -22,4 +22,16 @@
  #bir değeri geri döndürür
  $dun = unserialize($d);
  #dosyaya yazma ve dosyadan okuma değeri yeniden kullanma işine yarar.
+
+
+<?php
+$metin = 'short int sayim = 4; int max = 430000000;';
+$desen = '/(int|short int) [a-zA-Z_]+ (=) [0-9]+(;)/';
+
+preg_match_all($desen,$metin,$sonuc);
+echo "<pre>";
+print_r($sonuc[0]);
+
+
 ?>
+
